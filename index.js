65991f4a6d4bf90d71d16eef70a32ad52ec02fa5
SadikRa <sadikrahman494@gmail.com>
2023-02-22 14:31:06 +0600

    frist commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// 1) How will you access Sophia’s secondary school location?
+
+
+let data = {
+  Sophia: {
+    id: 33,
+    study: [
+      {
+        primary: [
+          { school_name: "ABC primary school" },
+          { location: "Peters burg" },
+        ],
+      },
+      {
+        secondary: [
+          { school_name: "ABC secondary school" },
+
+          { location: "St Lorence" },
+        ],
+      },
+    ],
+  },
+};
+
+
+let result = data.Sophia.study[1].secondary[1].location;
+console.log(result)
